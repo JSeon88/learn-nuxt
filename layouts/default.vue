48e9489348f463d2fbd9df3f1a45dc263b76d4b0
sunny <seonyoung.jeon@injeinc.co.kr>
2023-03-28 14:42:16 +0900

    layouts : 공통 페이지

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Nuxt 샘플 페이지</h1>
+        <Nuxt />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>없는 페이지입니다.</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
