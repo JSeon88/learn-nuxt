612bfd579ce394dc051a4ff3b27e57570bdf89ae
sunny <seonyoung.jeon@injeinc.co.kr>
2023-03-29 10:10:42 +0900

    axios 추가

---
+++ pages/main.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>메인 페이지</p>
+    <div>
+      {{ products }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      products: [],
+    }
+  },
+
+  async created() {
+    const res = await axios.get('http://localhost:3000/products')
+    this.products = res
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
