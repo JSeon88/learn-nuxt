bf8f979f4a85c4abe3b21028935402ef4f4cf650
sunny <seonyoung.jeon@injeinc.co.kr>
2023-03-29 10:16:24 +0900

    NuxtLink : 라우터 이동

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,13 @@
 <template>
-    <div>
-        <h1>Nuxt 샘플 페이지</h1>
-        <Nuxt />
-    </div>
+  <div>
+    <h1>Nuxt 샘플 페이지</h1>
+    <NuxtLink to="/">홈페이지</NuxtLink>
+    <NuxtLink to="/main">메인페이지</NuxtLink>
+    <NuxtLink to="/product">상품페이지</NuxtLink>
+    <Nuxt />
+  </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
