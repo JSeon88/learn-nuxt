0b3782975d4781bd532f8d2aa930ea49c0ef1bbb
sunny <seonyoung.jeon@injeinc.co.kr>
2023-03-29 14:28:44 +0900

    asyncData : Nuxt 데이터 호출 방식

--- README.md
+++ README.md
@@ -10,3 +10,12 @@
   - 버전 : 0.21.1
   - 1.x 버전 이슈 : require() of ES Module 이슈 있음
     [참고](https://github.com/axios/axios/issues/5091)
+
+## nuxt
+
+### asyncData
+
+- pages 폴더 안에서만 사용 가능
+- this를 사용한 데이터나 메소드 접근 불가
+  - vue 인스턴스가 아닌 Nuxt.js의 컨텍스트 객체이기 때문
+- asyncData에서 반환한 객체가 Vue 인스턴스의 data에 병합
--- pages/main.vue
+++ pages/main.vue
@@ -11,16 +11,27 @@
 import axios from 'axios'
 
 export default {
-  data() {
-    return {
-      products: [],
-    }
-  },
-
-  async created() {
+  // 페이지 진입하기 전에 호출
+  async asyncData() {
     const res = await axios.get('http://localhost:3000/products')
-    this.products = res
+    const products = res.data
+    // this는 Vue 인스턴스가 아닌 Nuxt.js의 컨텍스트 객체
+    // 따라서 this 접근 불가
+    // this.products = res.data
+
+    // asyncData에서 데이터를 가져온 후에는 asyncData에서 반환한 객체가 Vue 인스턴스의 data에 병합
+    return { products }
   },
+  // data() {
+  //   return {
+  //     products: [],
+  //   }
+  // },
+
+  // async created() {
+  //   const res = await axios.get('http://localhost:3000/products')
+  //   this.products = res.data
+  // },
 }
 </script>
 
