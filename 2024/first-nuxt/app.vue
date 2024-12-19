207bf55fbd83d27ac037f2ded377a8f73a6a894d
young <tjsud79@naver.com>
2024-12-19 16:41:36 +0900

    feat: Pages 사용 테스트

--- 2024/first-nuxt/app.vue
+++ 2024/first-nuxt/app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Main</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <!-- 추가 -->
+    <NuxtPage />
   </div>
 </template>
---
+++ 2024/first-nuxt/pages/blog/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Blog Main Page</h1>
+    </div>
+</template>
