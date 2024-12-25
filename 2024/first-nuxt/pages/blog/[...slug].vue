3c39b455eff7c86c99047a3b0d37bb42a7f3a4ad
young <tjsud79@naver.com>
2024-12-26 01:48:23 +0900

    feat: NuxtContent - markdown and Typography

---
+++ 2024/first-nuxt/pages/blog/[...slug].vue
@@ -0,0 +1,11 @@
+<template>
+    <article class="prose dark:prose-invert">
+        <ContentDoc />
+    </article>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+console.log(route.path);
+</script>
