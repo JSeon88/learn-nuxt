9a14c1dde102f973208482774af432c4ed774e03
young <tjsud79@naver.com>
2024-12-27 01:29:40 +0900

    feat: NuxtContent - querying

--- 2024/first-nuxt/pages/blog/index.vue
+++ 2024/first-nuxt/pages/blog/index.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-        <h1>All blog posts list!</h1>
-    </div>
+    <section>
+        <ul>
+            <li v-for="post in posts" :key="post._path">
+                <NuxtLink :to="post._path">
+                    {{ post.title }}
+                </NuxtLink>
+            </li>
+        </ul>
+    </section>
 </template>
+
+<script setup lang="ts">
+const {data: posts} = await useAsyncData('blog-list', () =>  queryContent('/blog').only(['_path', 'title']).find());
+console.log(posts);
+</script>
