981c24fe74b17dfc48271fcfbb5ef69e549e0ab4
young <tjsud79@naver.com>
2025-01-29 17:56:54 +0900

    feat: NuxtContent - toc(목차)

---
+++ 2024/first-nuxt/components/content/toc-links.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul>
+        <li v-for="link in links" :key="link.id">
+            <NuxtLink :to="{path: route.path, hash: `#${link.id}`}" :class="{'ml-4': level}">
+                {{ link.text }}
+            </NuxtLink>
+            <TocLinks :links="link.children" :level="level + 1" />
+        </li>
+    </ul>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+type LinkType = {
+    id: string,
+    text: string,
+    children?: LinkType[]
+}
+
+defineProps({
+    links: Array<LinkType>,
+    level: {
+        type: Number,
+        default: 0
+    }
+})
+</script>
