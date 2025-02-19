075ccde4a421f825e6386137e0b50e19f6932ac9
전선영 <jeonseon-yeong@jeonseon-yeong-ui-MacBookPro-2.local>
2025-02-19 20:12:14 +0900

    feat: NuxtContent - slots : not-found

--- 2024/first-nuxt/pages/blog/[...slug].vue
+++ 2024/first-nuxt/pages/blog/[...slug].vue
@@ -1,21 +1,29 @@
 <template>
     <article class="prose dark:prose-invert max-w-none prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300">
-        <ContentDoc v-slot="{doc}">
-            <div class="grid grid-cols-6 gap-16">
-                <div :class="{'col-span-4': doc.toc, 'col-span-6': !doc.toc}">
-                    <ContentRenderer :value="doc" />
+        <ContentDoc>
+            <template v-slot="{doc}">
+                <div class="grid grid-cols-6 gap-16">
+                    <div :class="{'col-span-4': doc.toc, 'col-span-6': !doc.toc}">
+                        <ContentRenderer :value="doc" />
+                    </div>
+                    <div class="col-span-2 not-prose" v-if="doc.toc">
+                        <aside class="sticky top-8">
+                            <div class="font-semibold mb-2">
+                                Table of Contents
+                            </div>
+                            <nav>
+                                <TocLinks :links="doc.body?.toc?.links" :active-id="activeId"/>
+                            </nav>
+                        </aside>
+                    </div>
                 </div>
-                <div class="col-span-2 not-prose" v-if="doc.toc">
-                    <aside class="sticky top-8">
-                        <div class="font-semibold mb-2">
-                            Table of Contents
-                        </div>
-                        <nav>
-                            <TocLinks :links="doc.body?.toc?.links" :active-id="activeId"/>
-                        </nav>
-                    </aside>
+            </template>
+            <template #not-found>
+                <div class="text-center">
+                    <h1 class="text-4xl font-bold">404</h1>
+                    <p class="text-lg">Page not found</p>
                 </div>
-            </div>
+            </template>
         </ContentDoc>
     </article>
 </template>
