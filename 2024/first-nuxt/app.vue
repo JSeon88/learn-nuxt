dfb90c30f48abaa8c16f11dbd04720cc2632c18b
seonyjeon <seonyjeon@smilegate.com>
2025-04-27 21:24:44 +0900

    feat: transition

--- 2024/first-nuxt/app.vue
+++ 2024/first-nuxt/app.vue
@@ -6,6 +6,14 @@
   </div>
 </template>
 
-<!-- <script setup lang="ts">
-const layout = 'another';
-</script> -->
+<style>
+ .page-enter-active,
+ .page-leave-active {
+   transition: all 0.3s;
+ }
+ .page-enter-from,
+ .page-leave-to {
+   opacity: 0.5;
+   filter: grayscale(1);
+ }
+ </style>
