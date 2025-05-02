739a5a90206262b9080541ec52ae92f106bcaeab
seonyjeon <seonyjeon@smilegate.com>
2025-05-02 19:51:08 +0900

    chore: tailwind responsive design 추가

--- 2024/first-nuxt/components/menu.vue
+++ 2024/first-nuxt/components/menu.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="font-mono">
-        <ul class="flex space-x-4">
+        <ul class="flex flex-col md:flex-row md:space-x-4">
           <li>
             <NuxtLink to="/" class="link">Main</NuxtLink>
           </li>
